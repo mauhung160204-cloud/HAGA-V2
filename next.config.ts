16070ef23f0e52dbc8cf99e4b7f629c18f489e3b
mauhung160204-cloud <mauhung160204@gmail.com>
2026-05-26 19:21:19 +0700

    Cap nhat next.config.ts de hien thi anh tu Yahoo

--- next.config.ts
+++ next.config.ts
@@ -4,9 +4,12 @@ const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
       {
-        protocol: "https",
-        hostname: "images.unsplash.com",
-        pathname: "/**",
+        protocol: 'https',
+        hostname: 'images.unsplash.com',
+      },
+      {
+        protocol: 'https',
+        hostname: 'images.search.yahoo.com', // Cấp phép thêm cho link ảnh từ Yahoo
       },
     ],
   },
